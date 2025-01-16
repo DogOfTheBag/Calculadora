8a286c61b4967b8ad8b25fda6e162dab76d9980f
DogOfTheBag <albercortijo@gmail.com>
2025-01-16 16:28:10 +0100

    Update Calc.java

--- src/Calc.java
+++ src/Calc.java
@@ -9,14 +9,6 @@ public class Calc
         this.num1 = num1;
         this.num2 = num2;
     }
-    /*public int getNum1()
-    {
-        return num1;
-    }
-    public int getNum2()
-    {
-        return num2;
-    } */
     public int suma()
     {
         return num1 + num2;
